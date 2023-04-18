a4b971ad4b71274afe7f6c23800f5c692e2d5375
Mateus Barcelos <mateuspit@hotmail.com>
2023-04-18 14:55:29 -0300

    git init, librarys done, same imports done, the begginig

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import apiPort from "../constants/apiPort.js";
+import { MongoClient, ObjectId } from 'mongodb';
+
+// console.log(apiPort);
